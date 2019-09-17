7c3eb10b70349e7bb716cb0d83c83b0c43873695
Devaraj Reddy <Devaraj.Reddy@logmein.com>
2019-09-17 08:17:43 -0700

    Send Location from one client to others use geoLocation browser api

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,10 +4,47 @@ client.on('message', (message) => {
     console.log(message);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('button');
+
+const $sendLocationButton = document.querySelector('#send-location');
+
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value;
 
-    client.emit('sendMessage', message);
+    client.emit('sendMessage', message, (error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value=''
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+        console.log('This message has been delivered')
+    });
+})
+
+$sendLocationButton.addEventListener('click', (e) => {
+    if(!navigator.geolocation){
+        return alert('GeoLocation is not supported by your browser!')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position);
+        client.emit('sendLocation', {
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        }, ()=>{
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location Shared!')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io')
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,12 +18,24 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!!');
     socket.broadcast.emit('message', 'A user has joined the chat!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+
+        const filter = new Filter();
+        if(filter.isProfane(message)){
+           return callback('Profanity is not allowed!!')
+        }
+
         io.emit('message', message);
+        callback()
     })
 
     socket.on('disconnect', ()=>{
         io.emit('message', 'A user has left the chat!')
+    })
+
+    socket.on('sendLocation', (coords, callback)=>{
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 })
 
